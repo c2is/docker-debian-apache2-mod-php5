c16f69f5841b5e23747cd820e142cc4b3fed12a2
André Cianfarani <acianfa@gmail.com>
2018-01-05 08:40:30 +0100

    Added ssl

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,15 @@ FROM debian:jessie
 MAINTAINER André Cianfarani <a.cianfarani@c2is.fr>
 
 RUN apt-get update && apt-get install -y apache2 php5 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php5-mcrypt php5-xmlrpc
-
+ENV APACHE_RUN_USER www-data
+ENV APACHE_RUN_GROUP www-data
+ENV APACHE_LOG_DIR /var/log/apache2
+RUN mkdir /etc/apache2/ssl
 RUN a2enmod rewrite
+RUN a2enmod ssl
+RUN a2enmod headers
 
+ADD ports.conf /etc/apache2/ports.conf
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod 777 /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -29,6 +29,37 @@ cat <<EOF >> /etc/apache2/sites-available/vhost-website.conf
 	      AllowOverride All
         </Directory>
 </VirtualHost>
+<VirtualHost *:443>
+        ServerName $WEBSITE_HOST
+        ServerAlias *.$WEBSITE_HOST
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/website$SUFFIX
+
+        ErrorLog ${APACHE_LOG_DIR}/error-ssl.log
+        CustomLog ${APACHE_LOG_DIR}/access-ssl.log combined
+
+        <Directory /var/www/website$SUFFIX>
+          Require all granted
+          Options -Indexes +FollowSymLinks -MultiViews
+          Order allow,deny
+          allow from all
+              AllowOverride All
+        </Directory>
+
+        SSLEngine on
+        SSLCertificateFile    /etc/apache2/ssl/ssl.crt
+        SSLCertificateKeyFile /etc/apache2/ssl/ssl.key
+        BrowserMatch "MSIE [2-6]" \
+                nokeepalive ssl-unclean-shutdown \
+                downgrade-1.0 force-response-1.0
+        BrowserMatch "MSIE [17-9]" ssl-unclean-shutdown
+        RequestHeader set X-Forwarded-Proto "https"
+</VirtualHost>
 EOF
+
+openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=FR/ST=c2is/L=Lyon/O=c2is/CN=$CERTIFICAT_CNAME" -keyout /etc/apache2/ssl/ssl.key -out /etc/apache2/ssl/ssl.crt
+
+exec "$@"
+
 
 exec "$@"
